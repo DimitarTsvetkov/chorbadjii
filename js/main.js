143b5155d42c8e2b12a119fa5bd9c0b33144b0ce
unknown <k.raychev@abv.bg>
2024-04-03 19:53:55 +0300

    fix videos on index

--- js/main.js
+++ js/main.js
@@ -77,10 +77,7 @@
     });
 
     $("#videoModal").on("shown.bs.modal", function (e) {
-      $("#video").attr(
-        "src",
-        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
-      );
+      $("#video").attr("src", $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
     });
 
     $("#videoModal").on("hide.bs.modal", function (e) {
